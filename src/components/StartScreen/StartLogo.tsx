9e8457945beb130acb4a45864b8d4bc9c637e739
Kuba <kuba.wasowicz@poczta.onet.pl>
2023-09-03 00:56:03 +0200

    fix path

--- src/components/StartScreen/StartLogo.tsx
+++ src/components/StartScreen/StartLogo.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-const logo: string = require("../../assets/logo.svg").default;
+const logo: string = require("../../assets/images/logo.svg").default;
 
 const StartLogo: FC = () => {
   return <img className="game-container__logo" src={logo} alt="logo" />;
