9f74d3d8b7635cd0128f89a3515b51f8c6ff0a67
Kuba <kuba.wasowicz@poczta.onet.pl>
2023-09-09 18:01:30 +0200

    fix spinner animation

--- src/components/EndScreen/EndScreenSpinners.tsx
+++ src/components/EndScreen/EndScreenSpinners.tsx
@@ -1,40 +1,170 @@
-import { FadeLoader } from "react-spinners";
-
 const EndScreenSpinners = () => {
   return (
-    <div className="end-screen__spinners-container">
-      <FadeLoader
-        color="#e7e7e7"
-        cssOverride={{
-          position: "absolute",
-          zIndex: 1,
-          left: "50%",
-          transform: "translate(-30%, -110%)",
-          opacity: 0.4,
-        }}
-        height={1000}
-        margin={100}
-        radius={1}
-        speedMultiplier={2}
-        width={50}
-      />
-
-      <FadeLoader
-        color="#e7e7e7"
-        cssOverride={{
-          position: "absolute",
-          zIndex: 1,
-          left: "50%",
-          transform: "translate(15%, -95%) rotate(22.5deg)",
-          opacity: 0.4,
-        }}
-        height={1000}
-        margin={100}
-        radius={1}
-        speedMultiplier={2}
-        width={50}
-      />
+    <div className="spinner">
+<svg
+      width="100%"
+      height="100vh"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(0, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(15, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(30, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(45, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(60, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(75, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(90, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(105, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(120, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(135, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(150, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(165, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(180, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(195, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(210, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(225, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(240, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(255, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(270, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(285, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(300, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(315, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#dfdfdf5e"
+          transform-origin="50 50"
+          transform="rotate(330, 0, 0)"
+        />
+        <path
+          d="M 50 50 L 250 50 L 243.18516525781365 101.76380902050414"
+          fill="#bebebe4b"
+          transform-origin="50 50"
+          transform="rotate(345, 0, 0)"
+        />
+        <animateTransform
+          attributeName="transform"
+          attributeType="XML"
+          type="rotate"
+          from="0 50 50"
+          to="360 50 50"
+          dur="10s"
+          repeatCount="indefinite"
+        />
+      </g>
+    </svg>
     </div>
+    
   );
 };
 
